067f8c34f047711ef09613cefca17d0b311a3074
Howy Perrin <howyp@users.noreply.github.com>
2017-12-09 18:14:52 +0000

    Tighten up the supported currency types

--- build.sbt
+++ build.sbt
@@ -5,17 +5,19 @@ name := "http4s-sandbox"
 version := "0.0.1-SNAPSHOT"
 scalaVersion := "2.12.4"
 
+val CirceVersion   = "0.8.0"
 val Http4sVersion  = "0.17.5"
 val Specs2Version  = "4.0.0"
 val LogbackVersion = "1.2.3"
 
 libraryDependencies ++= Seq(
-  "org.http4s"     %% "http4s-blaze-server" % Http4sVersion,
-  "org.http4s"     %% "http4s-circe"        % Http4sVersion,
-  "org.http4s"     %% "http4s-dsl"          % Http4sVersion,
-  "io.circe"       %% "circe-generic"       % "0.8.0",
-  "io.circe"       %% "circe-literal"       % "0.8.0",
-  "ch.qos.logback" % "logback-classic"      % LogbackVersion,
-  "org.scalatest"  %% "scalatest"           % "3.0.4" % Test,
-  "org.scalacheck" %% "scalacheck"          % "1.13.4" % Test
+  "org.http4s"     %% "http4s-blaze-server"  % Http4sVersion,
+  "org.http4s"     %% "http4s-circe"         % Http4sVersion,
+  "org.http4s"     %% "http4s-dsl"           % Http4sVersion,
+  "io.circe"       %% "circe-generic"        % CirceVersion,
+  "io.circe"       %% "circe-generic-extras" % CirceVersion,
+  "io.circe"       %% "circe-literal"        % CirceVersion,
+  "ch.qos.logback" % "logback-classic"       % LogbackVersion,
+  "org.scalatest"  %% "scalatest"            % "3.0.4" % Test,
+  "org.scalacheck" %% "scalacheck"           % "1.13.4" % Test
 )
--- src/main/scala/com/example/http4ssandbox/Offers.scala
+++ src/main/scala/com/example/http4ssandbox/Offers.scala
@@ -1,24 +1,28 @@
 package com.example.http4ssandbox
 
-import java.time.Instant
-
 import fs2.Task
 import io.circe._
+import io.circe.generic.extras.semiauto.{deriveEnumerationDecoder, deriveEnumerationEncoder}
 import io.circe.generic.semiauto._
 import io.circe.syntax._
 import org.http4s._
 import org.http4s.circe._
-import org.http4s.server._
 import org.http4s.dsl._
 
 object Offers {
-  case class Price(currency: String, amount: Long)
+  sealed trait CurrencyAmount
+  object CurrencyAmount {
+    case object GBp extends CurrencyAmount
+  }
+  case class Price(currency: CurrencyAmount, amount: Long)
   case class Offer(merchantId: Long, productId: String, price: Price)
 
-  implicit val priceDecoder: Decoder[Price] = deriveDecoder[Price]
-  implicit val priceEncoder: Encoder[Price] = deriveEncoder[Price]
-  implicit val offerDecoder: Decoder[Offer] = deriveDecoder[Offer]
-  implicit val offerEncoder: Encoder[Offer] = deriveEncoder[Offer]
+  implicit val currencyAmountDecoder: Decoder[CurrencyAmount] = deriveEnumerationDecoder[CurrencyAmount]
+  implicit val currencyAmountEncoder: Encoder[CurrencyAmount] = deriveEnumerationEncoder[CurrencyAmount]
+  implicit val priceDecoder: Decoder[Price]                   = deriveDecoder[Price]
+  implicit val priceEncoder: Encoder[Price]                   = deriveEncoder[Price]
+  implicit val offerDecoder: Decoder[Offer]                   = deriveDecoder[Offer]
+  implicit val offerEncoder: Encoder[Offer]                   = deriveEncoder[Offer]
 
   var currentOffers: IndexedSeq[Offer] = IndexedSeq.empty
 
