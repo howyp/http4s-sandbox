726fd3005522c2ba7729909002e4ae07d48a0638
Howy Perrin <howyp@users.noreply.github.com>
2017-12-09 17:28:11 +0000

    Created offers now have their own URL (naive impl)

--- src/main/scala/com/example/http4ssandbox/Offers.scala
+++ src/main/scala/com/example/http4ssandbox/Offers.scala
@@ -2,6 +2,7 @@ package com.example.http4ssandbox
 
 import java.time.Instant
 
+import fs2.Task
 import io.circe._
 import io.circe.generic.semiauto._
 import io.circe.syntax._
@@ -17,11 +18,12 @@ object Offers {
   var currentOffers: IndexedSeq[Offer]      = IndexedSeq.empty
 
   val service = HttpService {
-    case GET -> Root / "offers" => Ok(currentOffers.asJson)
+    case GET -> Root / "offers"      => Ok(currentOffers.asJson)
+    case GET -> Root / "offers" / id => Ok(currentOffers.head.asJson)
     case req @ POST -> Root / "offers" =>
       req.as(jsonOf[Offer]).flatMap { offer =>
         currentOffers = currentOffers :+ offer
-        Created()
+        Task.now(Response(Created, headers = Headers(headers.Location(uri("/offers/1")))))
       }
   }
 }
--- src/test/scala/com/example/http4ssandbox/OffersSpec.scala
+++ src/test/scala/com/example/http4ssandbox/OffersSpec.scala
@@ -3,7 +3,8 @@ package com.example.http4ssandbox
 import fs2.Task
 import io.circe._
 import io.circe.literal._
-import org.http4s._
+import org.http4s.{Method, _}
+import Method.{GET, POST}
 import org.http4s.circe._
 import org.http4s.dsl._
 import org.scalatest.OptionValues._
@@ -13,21 +14,29 @@ import org.scalatest.{FreeSpec, Matchers}
 class OffersSpec extends FreeSpec with Matchers {
   "Offers" - {
     "can be created by a merchant" in {
-      responseTo(Request(Method.GET, uri("/offers"))) should have(status(Status.Ok), body(json"""[]"""))
+      responseTo(Request(GET, uri("/offers"))) should have(status(Status.Ok), body(json"""[]"""))
 
-      val offerToCreate = json"""{ "merchantId": 1234 }"""
-      responseTo(Request(Method.POST, uri("/offers")).withBody(offerToCreate)) should have(status(Status.Created))
+      val postResponse = responseTo(Request(POST, uri("/offers")).withBody(json"""{ "merchantId": 1234 }"""))
+      postResponse should have(status(Status.Created), header(headers.Location))
 
-      responseTo(Request(Method.GET, uri("/offers"))) should have(
+      responseTo(Request(GET, postResponse.headers.get(headers.Location).value.uri)) should have(
         status(Status.Ok),
-        body(json"""[{ "merchantId": 1234 }]""")
-      )
+        body(json"""{ "merchantId": 1234 }"""))
+
+      responseTo(Request(GET, uri("/offers"))) should have(status(Status.Ok), body(json"""[{ "merchantId": 1234 }]"""))
     }
   }
-  def responseTo(request: Request)       = Offers.service(request).unsafeRun().toOption.value
-  def responseTo(request: Task[Request]) = request.flatMap(Offers.service(_)).unsafeRun().toOption.value
+  def responseTo(request: Request)       = Offers.service.orNotFound(request).unsafeRun()
+  def responseTo(request: Task[Request]) = request.flatMap(Offers.service.orNotFound(_)).unsafeRun()
+
   def status(expected: Status) = HavePropertyMatcher { (response: Response) =>
     HavePropertyMatchResult(response.status == expected, "status", expected, response.status)
+  }
+  def header(expected: HeaderKey.Extractable) = HavePropertyMatcher { (response: Response) =>
+    HavePropertyMatchResult(response.headers.get(expected).isDefined,
+                            "header keys",
+                            "containing " + expected.name,
+                            response.headers.map(_.name))
   }
   def body(expected: Json) = HavePropertyMatcher { (response: Response) =>
     val actual = response.as[Json].unsafeRun()
