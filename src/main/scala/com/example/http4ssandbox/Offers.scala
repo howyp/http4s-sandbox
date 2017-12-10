e2f0d040122661737eca31a8271b0ebadfdfc9cc
Howy Perrin <howyp@users.noreply.github.com>
2017-12-10 19:51:43 +0000

    Use optional query params to simplify routing

--- src/main/scala/com/example/http4ssandbox/Offers.scala
+++ src/main/scala/com/example/http4ssandbox/Offers.scala
@@ -18,18 +18,18 @@ class Offers {
   private val offerCollectionItem: ((Int, Offer)) => Json = {
     case (id, offer) => Json.obj("href" -> offerUri(id).asJson, "item" -> offer.asJson)
   }
+  private def orderMatches(maybeMerchantId: Option[Long], maybeProductId: Option[String]): ((Int, Offer)) => Boolean = {
+    case (_, offer) =>
+      maybeMerchantId.forall(_ === offer.merchantId) &&
+        maybeProductId.forall(_ === offer.productId)
+  }
 
-  object MerchantId extends QueryParamDecoderMatcher[Long]("merchantId")
-  object ProductId  extends QueryParamDecoderMatcher[String]("productId")
+  object MerchantId extends OptionalQueryParamDecoderMatcher[Long]("merchantId")
+  object ProductId  extends OptionalQueryParamDecoderMatcher[String]("productId")
 
   val service = HttpService {
-    case GET -> Root / "offers" :? MerchantId(mId) +& ProductId(pId) =>
-      Ok(currentOffers.filter(o => o._2.merchantId === mId && o._2.productId === pId).map(offerCollectionItem).asJson)
-    case GET -> Root / "offers" :? MerchantId(mId) =>
-      Ok(currentOffers.filter(_._2.merchantId === mId).map(offerCollectionItem).asJson)
-    case GET -> Root / "offers" :? ProductId(pId) =>
-      Ok(currentOffers.filter(_._2.productId === pId).map(offerCollectionItem).asJson)
-    case GET -> Root / "offers"              => Ok(currentOffers.map(offerCollectionItem).asJson)
+    case GET -> Root / "offers" :? MerchantId(merchant) +& ProductId(product) =>
+      Ok(currentOffers.filter(orderMatches(merchant, product)).map(offerCollectionItem).asJson)
     case GET -> Root / "offers" / IntVar(id) => currentOffers.get(id).map(o => Ok(o.asJson)).getOrElse(Gone())
     case DELETE -> Root / "offers" / IntVar(id) =>
       currentOffers = currentOffers - id
